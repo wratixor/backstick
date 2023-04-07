9d9351f2b572068cabf91460f2c871f91608dda3
wratixor <83814888+wratixor@users.noreply.github.com>
2023-04-08 01:09:43 +0300

    Recipe all

--- src/main/java/com/github/wratixor/backstick/datagen/GenRecipe.java
+++ src/main/java/com/github/wratixor/backstick/datagen/GenRecipe.java
@@ -21,12 +21,13 @@ public class GenRecipe extends RecipeProvider {
     public GenRecipe(DataGenerator generator) {
         super(generator);
     }
+
     @Override
     protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
 
         // wood stick rotate
-        ShapelessRecipeBuilder.shapeless(RegMod.WOODEN_BACKSTICK.get()).requires(Tags.Items.RODS_WOODEN).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
-        ShapelessRecipeBuilder.shapeless(Items.STICK).requires(RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(RegMod.WOODEN_BACKSTICK.get(), 1).requires(Tags.Items.RODS_WOODEN).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.STICK, 1).requires(RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
         // other stick
         ShapedRecipeBuilder.shaped(RegMod.STONE_BACKSTICK.get(), 4).pattern("i").pattern("i").define('i', ItemTags.STONE_TOOL_MATERIALS).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
         ShapedRecipeBuilder.shaped(RegMod.COPPER_BACKSTICK.get(), 4).pattern("i").pattern("i").define('i', Tags.Items.INGOTS_COPPER).group(BSMODID).unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN)).save(consumer);
@@ -63,22 +64,72 @@ public class GenRecipe extends RecipeProvider {
         netheriteSmithing(consumer, RegMod.DIAMOND_BACKENGRAVER.get(), RegMod.NETHERITE_BACKENGRAVER.get());
         netheriteSmithing(consumer, RegMod.DIAMOND_BACKMUTATOR.get(), RegMod.NETHERITE_BACKMUTATOR.get());
         // torcherizing
-        ShapedRecipeBuilder.shaped(Blocks.TORCH,   4).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.TORCH,   2).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.STONE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.TORCH,  16).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.COPPER_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.TORCH,  32).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.IRON_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.TORCH,  64).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.GOLDEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.TORCH, 4).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.TORCH, 2).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.STONE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.TORCH, 16).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.COPPER_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.TORCH, 32).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.IRON_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.TORCH, 64).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.GOLDEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
         ShapedRecipeBuilder.shaped(Blocks.TORCH, 128).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.DIAMOND_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
         ShapedRecipeBuilder.shaped(Blocks.TORCH, 512).pattern("c").pattern("i").define('c', ItemTags.COALS).define('i', RegMod.NETHERITE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH,   4).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.WOODEN_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH,   2).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.STONE_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH,  16).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.COPPER_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH,  32).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.IRON_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH,  64).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.GOLDEN_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 1).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 2).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.STONE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 4).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.COPPER_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 8).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.IRON_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 16).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.GOLDEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 32).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.DIAMOND_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.REDSTONE_TORCH, 64).pattern("c").pattern("i").define('c', Tags.Items.DUSTS_REDSTONE).define('i', RegMod.NETHERITE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 4).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.WOODEN_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 2).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.STONE_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 16).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.COPPER_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 32).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.IRON_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 64).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.GOLDEN_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
         ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 128).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.DIAMOND_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
         ShapedRecipeBuilder.shaped(Blocks.SOUL_TORCH, 512).pattern("c").pattern("i").pattern("s").define('c', ItemTags.COALS).define('i', RegMod.NETHERITE_BACKSTICK.get()).define('s', ItemTags.SOUL_FIRE_BASE_BLOCKS).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
-        // backengraver recipes
-        
+        // ladderizing
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 3).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.WOODEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 2).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.STONE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 8).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.COPPER_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 16).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.IRON_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 32).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.GOLDEN_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 64).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.DIAMOND_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        ShapedRecipeBuilder.shaped(Blocks.LADDER, 128).pattern("i i").pattern("iii").pattern("i i").define('i', RegMod.NETHERITE_BACKSTICK.get()).group(BSMODID).unlockedBy("has_backstick", has(RegMod.ANY_BACKSTICK)).save(consumer);
+        // backbreacker recipes
+        ShapelessRecipeBuilder.shapeless(Items.BRICK, 4).requires(RegMod.ANY_BACKBREACKER).requires(ItemTags.TERRACOTTA).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.BRICK, 4).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.BRICKS).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.GLOWSTONE_DUST, 4).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.GLOWSTONE).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.SAND, 2).requires(RegMod.ANY_BACKBREACKER).requires(Tags.Items.GRAVEL).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.GRAVEL, 2).requires(RegMod.ANY_BACKBREACKER).requires(Tags.Items.COBBLESTONE).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.GRAVEL, 3).requires(RegMod.ANY_BACKBREACKER).requires(Tags.Items.STONE).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.CLAY_BALL, 4).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.CLAY).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.MELON_SLICE, 9).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.MELON).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.STRING, 4).requires(RegMod.ANY_BACKBREACKER).requires(ItemTags.WOOL).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.PRISMARINE_SHARD, 4).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.PRISMARINE).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.PRISMARINE_SHARD, 9).requires(RegMod.ANY_BACKBREACKER).requires(Blocks.PRISMARINE_BRICKS).group(BSMODID).unlockedBy("has_backbreacker", has(RegMod.ANY_BACKBREACKER)).save(consumer);
+        // backengravier recipe
+        ShapedRecipeBuilder.shaped(Blocks.SKELETON_SKULL, 1).pattern("bbb").pattern("beb").pattern("bbb").define('b', Blocks.BONE_BLOCK).define('e', RegMod.ANY_BACKENGRAVER).group(BSMODID).unlockedBy("has_backengraver", has(RegMod.ANY_BACKENGRAVER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.CHEST, 1).requires(RegMod.ANY_BACKENGRAVER).requires(ItemTags.LOGS).requires(ItemTags.LOGS).group(BSMODID).unlockedBy("has_backengraver", has(RegMod.ANY_BACKENGRAVER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.BARREL, 2).requires(RegMod.ANY_BACKENGRAVER).requires(ItemTags.LOGS).requires(ItemTags.LOGS).requires(ItemTags.LOGS).group(BSMODID).unlockedBy("has_backengraver", has(RegMod.ANY_BACKENGRAVER)).save(consumer);
+        // backmutator recipe
+        ShapelessRecipeBuilder.shapeless(Blocks.TERRACOTTA, 1).requires(RegMod.ANY_BACKMUTATOR).requires(ItemTags.TERRACOTTA).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.WHITE_WOOL, 1).requires(RegMod.ANY_BACKMUTATOR).requires(ItemTags.WOOL).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.MELON, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Blocks.PUMPKIN).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.PUMPKIN, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Blocks.MELON).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.FLINT, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.GRAVEL).requires(Tags.Items.GRAVEL).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapedRecipeBuilder.shaped(Items.FLINT, 6).pattern("bbb").pattern("bmb").pattern("bbb").define('b', Tags.Items.GRAVEL).define('m', RegMod.ANY_BACKMUTATOR).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.LEATHER, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Items.ROTTEN_FLESH).requires(Tags.Items.GRAVEL).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapedRecipeBuilder.shaped(Items.LEATHER, 6).pattern("bbb").pattern("bmb").pattern("bbb").define('b', Items.ROTTEN_FLESH).define('m', RegMod.ANY_BACKMUTATOR).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapedRecipeBuilder.shaped(Items.EXPERIENCE_BOTTLE, 2).pattern("dbd").pattern("bmb").pattern("ebe").define('b', Items.GLASS_BOTTLE).define('d', Tags.Items.GEMS_DIAMOND).define('e', Tags.Items.GEMS_EMERALD).define('m', RegMod.ANY_BACKMUTATOR).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.GHAST_TEAR, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.NUGGETS_IRON).requires(Tags.Items.GUNPOWDER).requires(Tags.Items.FEATHERS).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.SLIME_BALL, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Items.GREEN_DYE).requires(Items.BONE_MEAL).requires(Items.WATER_BUCKET).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        // heads
+        ShapelessRecipeBuilder.shapeless(Blocks.SKELETON_SKULL, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.HEADS).requires(Tags.Items.BONES).requires(Tags.Items.BONES).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.ZOMBIE_HEAD, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.HEADS).requires(Items.ROTTEN_FLESH).requires(Items.ROTTEN_FLESH).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.CREEPER_HEAD, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.HEADS).requires(Tags.Items.GUNPOWDER).requires(Tags.Items.GUNPOWDER).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.WITHER_SKELETON_SKULL, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Tags.Items.HEADS).requires(Items.WITHER_ROSE).requires(Items.WITHER_ROSE).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Blocks.DRAGON_HEAD, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Blocks.SKELETON_SKULL).requires(Blocks.ZOMBIE_HEAD).requires(Blocks.CREEPER_HEAD).requires(Blocks.WITHER_SKELETON_SKULL).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(Items.DRAGON_EGG, 1).requires(RegMod.ANY_BACKMUTATOR).requires(Blocks.DRAGON_HEAD).group(BSMODID).unlockedBy("has_backmutator", has(RegMod.ANY_BACKMUTATOR)).save(consumer);
+
+
 
 
 
